b63377301b45ddde0d13430a4679f7660f455d28
Chaitanya Sharma <chetanyas11.cs@gmail.com>
2020-09-05 15:56:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import turtle
+chaitanya = turtle.Turtle()
+def square():
+  chaitanya.forward(100)
+  chaitanya.right(90)
+  chaitanya.forward(100)
+  chaitanya.right(90)
+  chaitanya.forward(100)
+  chaitanya.right(90)
+  chaitanya.forward(100)
+  #chaitanya.right(90)
+
+square()
+chaitanya.forward(200)
+square()
