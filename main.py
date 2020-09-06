b6d3080ee2185a720b2d97c32658fafe1fefaf13
Chaitanya Sharma <chetanyas11.cs.cs@gmail.com>
2020-09-06 03:50:38 +0000

    Calendar and if else statement

--- main.py
+++ main.py
@@ -1,3 +1,52 @@
+## turtle
+  # import turtle
+  # chaitanya = turtle.Turtle()
+  # def square():
+  #   chaitanya.forward(100)
+  #   chaitanya.right(90)
+  #   chaitanya.forward(100)
+  #   chaitanya.right(90)
+  #   chaitanya.forward(100)
+  #   chaitanya.right(90)
+  #   chaitanya.forward(100)
+  #   #chaitanya.right(90)
+
+  # square()
+  # chaitanya.forward(200)
+  # square()
+##
+
+## Calendar
+  # import calendar
+  # import datetime
+  # import time
+
+
+  # print(calendar.weekheader(2))
+  # print()
+
+  # print(calendar.firstweekday())
+  # print()
+
+  # print(calendar.month(2020,9))
+  # print()
+
+  # print(calendar.monthcalendar(2020,9))
+  # print()
+
+  # print(calendar.calendar(2020))
+  # print()
+
+  # print(calendar.weekday(2020,12,8))
+  # print()
+
+  # print(calendar.isleap(2100))
+  # print()
+
+  # print(calendar.leapdays(2000,2021))
+  # print()
+##
+
 import turtle
 chaitanya = turtle.Turtle()
 def square():
@@ -10,6 +59,10 @@ def square():
   chaitanya.forward(100)
   #chaitanya.right(90)
 
-square()
-chaitanya.forward(200)
-square()
+chaitan = 100
+qazi = 50
+
+if chaitan>qazi:
+  square()
+else :
+  chaitan.forward(100)
