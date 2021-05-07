cd59d45a1a6514b6a48a85d6c56bb897327ace63
Chaitanya Sharma <chetanyas11.cs.cs@gmail.com>
2021-05-07 17:34:41 +0530

    type of array using numpy file added

---
+++ Codes/type_of_array_using_numpy.py
@@ -0,0 +1,34 @@
+from numpy import   *
+#1. array()
+
+# arr = array([1, 2, 3, 4, 5])
+#
+# print(arr.dtype)
+# print(arr)
+
+#2. linspace
+
+# arr = linspace(0, 15)
+# print(arr)
+
+#3. arange()
+
+# arr = arange(1, 16, 2)
+# print(arr)
+
+#4. logspace()
+
+# arr = logspace(1,40,5)
+# print("%.2f" %arr[4])
+# print(arr)
+
+#5. zeros()
+
+# arr = zeros(5,int)
+# print(arr)
+
+#6. ones()
+arr = ones(5,int)
+print(arr)
+
+
